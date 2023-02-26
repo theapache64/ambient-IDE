b71bd2b86c9f94dabc711f03fa6fdd50936d38e9
theapache64 <theapache64@users.noreply.github.com>
2023-02-26 14:57:45 +0530

    🌟 add dyn log file path

--- src/main/kotlin/com/github/theapache64/ambientide/Main.kt
+++ src/main/kotlin/com/github/theapache64/ambientide/Main.kt
@@ -1,22 +1,22 @@
 package com.github.theapache64.ambientide
 
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
-import kotlinx.coroutines.runBlocking
+import java.io.File
 
 // For instant debugging (without assemble and load)
-fun main(args: Array<String>) = runBlocking {
+fun main(args: Array<String>) {
     App.args = args
     App().onCreate()
 }
 
 class MainComponent : StartupActivity, DumbAware {
     override fun runActivity(project: Project) {
-        println("App started~!!! $project")
-        App.args = arrayOf(
-            "/Users/theapache64/Library/Logs/Google/AndroidStudio2021.3/idea.log"
-        )
+        val logPath = "${PathManager.getLogPath()}${File.separator}idea.log"
+        println("App started~!!! $logPath")
+        App.args = arrayOf(logPath)
         App().onCreate()
     }
 }
